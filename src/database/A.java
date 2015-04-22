cfd01d3ef086eca473b3f0976835661b1a3516e9
stefanoantonel <stefanoantonel@gmail.com>
2015-04-21 21:17:52 -0500

    Improvements using methods

--- src/database/A.java
+++ src/database/A.java
@@ -10,51 +10,51 @@ public class A {
 	private static ResultSet rs;
 
 	public static void main(String args[]) {
-
-		try {
-			Class.forName("org.sqlite.JDBC");
-			dbName = "src/database/test.db";
-			c = DriverManager.getConnection("jdbc:sqlite:" + dbName);
-
-			dropTable("MyTable");
-			stmt = c.createStatement();
-			
-			String create = "CREATE TABLE MyTable(firstCol INTEGER)";
-			stmt.execute(create);
-			rs = stmt.getResultSet();
-//			System.out.println(rs.toString());
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
-		System.out.println("Opened database successfully");
+		openConnection();
+		dropTable("MyTable");		
+		createTable();
+		closeConnection();
 	}
-
-//	private static void createTable (String tableName) {
-////		String dropIf = "DROP TABLE IF EXISTS " + tableName;
-////		String createIf = "CREATE TABLE IF NOT EXISTS " + tableName ; 
-//		
-//		try {
-//			String createIf = "CREATE TABLE IF NOT EXISTS " + tableName + "AS " + tableName; 
-//			stmt.execute(createIf);
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
 	private static void dropTable(String tableName) {
 		try {
 			stmt = c.createStatement();
 			String drop = "DROP TABLE IF EXISTS " + tableName;
-			
-			stmt.execute(drop);
-						
+			stmt.execute(drop);		
 			rs = stmt.getGeneratedKeys();
-//			System.out.println(rs.toString());
-						
-//			stmt.close(); // close the Statement object
-//			c.close(); // close the Connection object
 		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}	
+	private static void closeConnection() {
+		try {
+			stmt.close(); // close the Statement object
+			c.close(); // close the Connection object
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	private static void openConnection() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			dbName = "src/database/test.db";
+			c = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	private static void createTable() {
+		try {
+			stmt = c.createStatement();
+			StringBuilder create = new StringBuilder();
+			create.append("CREATE TABLE MyTable(");
+			create.append("ID INTEGER,");
+			create.append("Name CHAR(20),");
+			create.append("Quantity INTEGER");
+			create.append(")");
+			stmt.execute(create.toString());
+			rs = stmt.getResultSet();	
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
